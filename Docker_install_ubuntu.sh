7e3cf361b662ec14957ca09f8125de58405ae575
Madhavan <157271254+M4dhavan@users.noreply.github.com>
2024-12-23 21:45:46 +0530

    Add files via upload

---
+++ Docker_install_ubuntu.sh
@@ -0,0 +1,6 @@
+#! /bin/bash                                                                                                            
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common                                                                                                                                                         sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+sudo snap install docker
+
+sudo apt update && sudo apt upgrade -y  
